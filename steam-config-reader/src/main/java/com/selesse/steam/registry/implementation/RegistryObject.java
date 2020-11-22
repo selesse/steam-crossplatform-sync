12e13d67df131d8b498064661eaff73c9907294a
Alex Selesse <alex@selesse.com>
2020-11-22 18:47:27 -0500

    Allow RegistryObjects to query dynamically too

--- steam-config-reader/src/main/java/com/selesse/steam/registry/implementation/RegistryObject.java
+++ steam-config-reader/src/main/java/com/selesse/steam/registry/implementation/RegistryObject.java
@@ -1,5 +1,6 @@
 package com.selesse.steam.registry.implementation;
 
+import com.google.common.base.Splitter;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
@@ -23,5 +24,30 @@ public class RegistryObject extends RegistryValue {
 
     public void put(String key, RegistryValue value) {
         values.put(key, value);
+    }
+
+    public RegistryObject getObjectValueAsObject(String path) {
+        return (RegistryObject) getObjectValue(path);
+    }
+
+    public RegistryString getObjectValueAsString(String path) {
+        return (RegistryString) getObjectValue(path);
+    }
+
+    public RegistryValue getObjectValue(String path) {
+        List<String> parts = Splitter.on("/").splitToList(path);
+
+        RegistryObject currentObject = this;
+
+        for (String part : parts) {
+            RegistryValue registryValue = currentObject.get(part);
+            if (registryValue instanceof RegistryObject) {
+                currentObject = (RegistryObject) registryValue;
+            } else {
+                return registryValue;
+            }
+        }
+
+        return currentObject;
     }
 }
--- steam-config-reader/src/main/java/com/selesse/steam/registry/implementation/RegistryStore.java
+++ steam-config-reader/src/main/java/com/selesse/steam/registry/implementation/RegistryStore.java
@@ -1,9 +1,5 @@
 package com.selesse.steam.registry.implementation;
 
-import com.google.common.base.Splitter;
-
-import java.util.List;
-
 public class RegistryStore {
     private final RegistryObject rootObject;
 
@@ -12,27 +8,14 @@ public class RegistryStore {
     }
 
     public RegistryObject getObjectValueAsObject(String path) {
-        return (RegistryObject) getObjectValue(path);
+        return rootObject.getObjectValueAsObject(path);
     }
 
     public RegistryString getObjectValueAsString(String path) {
-        return (RegistryString) getObjectValue(path);
+        return rootObject.getObjectValueAsString(path);
     }
 
     public RegistryValue getObjectValue(String path) {
-        List<String> parts = Splitter.on("/").splitToList(path);
-
-        RegistryObject currentObject = rootObject;
-
-        for (String part : parts) {
-            RegistryValue registryValue = currentObject.get(part);
-            if (registryValue instanceof RegistryObject) {
-                currentObject = (RegistryObject) registryValue;
-            } else {
-                return registryValue;
-            }
-        }
-
-        return currentObject;
+        return rootObject.getObjectValue(path);
     }
 }
