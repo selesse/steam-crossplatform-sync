3ef4590a7b2891839bde0477a9953eb751e7aef3
Alex Selesse <alex@selesse.com>
2020-11-29 17:44:42 -0500

    Add localOsRelease support for OS X
    This enhances the existing release task to create a plist file that will
    launch the syncer automatically.
    
    Launching the plist is left as a manual task for now.

--- app/src/main/java/com/selesse/steam/crossplatform/sync/config/SteamCrossplatformSyncMacConfig.java
+++ app/src/main/java/com/selesse/steam/crossplatform/sync/config/SteamCrossplatformSyncMacConfig.java
@@ -1,10 +1,13 @@
 package com.selesse.steam.crossplatform.sync.config;
 
 import java.nio.file.Path;
+import java.util.Optional;
 
 public class SteamCrossplatformSyncMacConfig implements SteamCrossplatformSyncConfig {
     @Override
     public Path getConfigDirectory() {
-        return Path.of(System.getProperty("user.home"), ".config", "steam-crossplatform-sync");
+        String configDirectory = Optional.ofNullable(System.getenv("XDG_CONFIG_HOME"))
+                .orElse(System.getProperty("user.home") + "/.config");
+        return Path.of(configDirectory, "steam-crossplatform-sync");
     }
 }
