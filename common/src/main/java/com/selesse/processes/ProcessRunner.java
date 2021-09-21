b66c53d73ca1a46fb4f2a29252deea9e172b7edc
Alex Selesse <alex@selesse.com>
2021-09-21 19:04:36 -0400

    Always include /usr/local/bin in path for processes

--- common/src/main/java/com/selesse/processes/ProcessRunner.java
+++ common/src/main/java/com/selesse/processes/ProcessRunner.java
@@ -1,6 +1,9 @@
 package com.selesse.processes;
 
+import com.selesse.os.OperatingSystems;
+
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
@@ -18,6 +21,10 @@ public class ProcessRunner {
 
     public String runAndGetOutput() {
         try {
+            if (OperatingSystems.get() == OperatingSystems.OperatingSystem.MAC) {
+                processBuilder.environment()
+                        .put("PATH", processBuilder.environment().get("PATH") + File.pathSeparator + "/usr/local/bin");
+            }
             Process process = processBuilder.start();
             BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
             StringBuilder output = new StringBuilder();
