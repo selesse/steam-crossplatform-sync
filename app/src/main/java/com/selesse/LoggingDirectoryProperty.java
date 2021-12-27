d6c1d43cda568f7be145041d6e164672f77c5a6d
Alex Selesse <alex@selesse.com>
2021-12-27 14:34:38 -0500

    Log to project if running in an IDE
    This prevents pollution of the actual logs when in development mode.

--- app/src/main/java/com/selesse/LoggingDirectoryProperty.java
+++ app/src/main/java/com/selesse/LoggingDirectoryProperty.java
@@ -1,13 +1,25 @@
 package com.selesse;
 
 import ch.qos.logback.core.PropertyDefinerBase;
+import com.google.common.base.StandardSystemProperty;
 import com.selesse.steam.crossplatform.sync.config.SteamCrossplatformSync;
 import com.selesse.steam.crossplatform.sync.config.SteamCrossplatformSyncConfig;
+
+import java.util.Objects;
 
 public class LoggingDirectoryProperty extends PropertyDefinerBase {
     @Override
     public String getPropertyValue() {
-        SteamCrossplatformSyncConfig config = SteamCrossplatformSync.loadConfiguration();
-        return config.getConfigDirectory().toAbsolutePath().toString();
+        if (runningInIde()) {
+            return StandardSystemProperty.USER_DIR.value() + "/log";
+        } else {
+            SteamCrossplatformSyncConfig config = SteamCrossplatformSync.loadConfiguration();
+            return config.getConfigDirectory().toAbsolutePath().toString();
+        }
+    }
+
+    private boolean runningInIde() {
+        String javaClassPath = StandardSystemProperty.JAVA_CLASS_PATH.value();
+        return Objects.requireNonNull(javaClassPath).contains("build/classes");
     }
 }
