361ce9ce8a7df654f87e1192f1e511269904bdd9
Alex Selesse <alex@selesse.com>
2022-02-02 23:43:31 -0500

    Make steamcmd work on OS X again

--- common/src/main/java/com/selesse/processes/ProcessRunner.java
+++ common/src/main/java/com/selesse/processes/ProcessRunner.java
@@ -6,7 +6,12 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 public class ProcessRunner {
     private final ProcessBuilder processBuilder;
@@ -21,10 +26,7 @@ public class ProcessRunner {
 
     public String runAndGetOutput() {
         try {
-            if (OperatingSystems.get() == OperatingSystems.OperatingSystem.MAC) {
-                processBuilder.environment()
-                        .put("PATH", processBuilder.environment().get("PATH") + File.pathSeparator + "/usr/local/bin");
-            }
+            replaceRelativePathWithAbsolutePath();
             Process process = processBuilder.start();
             BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
             StringBuilder output = new StringBuilder();
@@ -37,5 +39,28 @@ public class ProcessRunner {
         } catch (InterruptedException | IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    // Java's ProcessBuilder doesn't let you modify the $PATH for the existing execution, so we have to manually look
+    // up the executable on the $PATH and replace it with its absolute path.
+    // On OS X, steamcmd wasn't being found since it was in /usr/local/bin and that's not on the default plist PATH...
+    private void replaceRelativePathWithAbsolutePath() {
+        List<String> existingArguments = processBuilder.command();
+        String programName = existingArguments.get(0);
+        String pathEnvironmentVariable = processBuilder.environment().get("PATH");
+        if (OperatingSystems.get() != OperatingSystems.OperatingSystem.WINDOWS) {
+            pathEnvironmentVariable += File.pathSeparator + "/usr/local/bin";
+        }
+        Path filePath = findAbsolutePathOfProgramInPath(programName, pathEnvironmentVariable);
+        existingArguments.set(0, filePath.toString());
+    }
+
+    private Path findAbsolutePathOfProgramInPath(String programName, String pathEnvironmentVariable) {
+        return Stream.of(pathEnvironmentVariable.split(Pattern.quote(File.pathSeparator)))
+                .map(Paths::get)
+                .map(path -> Path.of(path.toString(), programName))
+                .filter(Files::exists)
+                .filter(Files::isExecutable)
+                .findFirst().orElseThrow(RuntimeException::new);
     }
 }
