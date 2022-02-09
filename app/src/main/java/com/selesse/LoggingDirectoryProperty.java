348220a4ae78cf5b00233e73c121004b766a9fdc
Alex Selesse <alex@selesse.com>
2022-02-08 19:45:08 -0500

    Fix IDE detection in Windows

--- app/src/main/java/com/selesse/LoggingDirectoryProperty.java
+++ app/src/main/java/com/selesse/LoggingDirectoryProperty.java
@@ -5,13 +5,14 @@ import com.google.common.base.StandardSystemProperty;
 import com.selesse.steam.crossplatform.sync.config.SteamCrossplatformSync;
 import com.selesse.steam.crossplatform.sync.config.SteamCrossplatformSyncConfig;
 
+import java.io.File;
 import java.util.Objects;
 
 public class LoggingDirectoryProperty extends PropertyDefinerBase {
     @Override
     public String getPropertyValue() {
         if (runningInIde()) {
-            return StandardSystemProperty.USER_DIR.value() + "/log";
+            return StandardSystemProperty.USER_DIR.value() + File.separatorChar + "log";
         } else {
             SteamCrossplatformSyncConfig config = SteamCrossplatformSync.loadConfiguration();
             return config.getConfigDirectory().toAbsolutePath().toString();
@@ -20,6 +21,6 @@ public class LoggingDirectoryProperty extends PropertyDefinerBase {
 
     private boolean runningInIde() {
         String javaClassPath = StandardSystemProperty.JAVA_CLASS_PATH.value();
-        return Objects.requireNonNull(javaClassPath).contains("build/classes");
+        return Objects.requireNonNull(javaClassPath).contains("build" + File.separatorChar + "classes");
     }
 }
