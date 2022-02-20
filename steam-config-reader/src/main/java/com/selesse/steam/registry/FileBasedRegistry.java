7420a7a57277b7b7c6b078e4e3b63b206de4ad93
Alex Selesse <alex@selesse.com>
2022-02-19 23:16:10 -0500

    Further simplify getGameMetadata

--- steam-config-reader/src/main/java/com/selesse/steam/registry/FileBasedRegistry.java
+++ steam-config-reader/src/main/java/com/selesse/steam/registry/FileBasedRegistry.java
@@ -1,6 +1,5 @@
 package com.selesse.steam.registry;
 
-import com.google.common.collect.Lists;
 import com.selesse.steam.AppType;
 import com.selesse.steam.SteamApp;
 import com.selesse.steam.SteamAppLoader;
@@ -43,28 +42,20 @@ public abstract class FileBasedRegistry extends SteamRegistry {
 
     public List<Long> getInstalledAppIds() {
         RegistryObject object = getAppsRegistryObject();
-        return object.getKeys().stream().map(Long::valueOf).sorted().collect(Collectors.toList());
+        return object.getKeys().stream().map(Long::valueOf)
+                .filter(this::isInstalled).sorted().collect(Collectors.toList());
     }
 
     public List<SteamGameMetadata> getGameMetadata() {
-        RegistryObject object = getAppsRegistryObject();
-        List<SteamGameMetadata> steamGames = Lists.newArrayList();
-        for (long gameId : object.getKeys().stream().map(Long::parseLong).collect(Collectors.toList())) {
-            if (getRegistryObject(gameId).pathExists("installed")) {
-                SteamApp steamApp = SteamAppLoader.load(gameId);
-
-                if (steamApp.getType() == AppType.GAME) {
-                    steamGames.add(getGameMetadata(gameId));
-                }
-            }
-        }
-        return steamGames;
+        return getInstalledAppIds().stream()
+                .map(this::getGameMetadata)
+                .filter(gameMetadata -> SteamAppLoader.load(gameMetadata.getGameId()).getType() == AppType.GAME)
+                .collect(Collectors.toList());
     }
 
     public SteamGameMetadata getGameMetadata(Long gameId) {
         SteamApp steamApp = SteamAppLoader.load(gameId);
-        String installedValue = getRegistryObject(gameId).getObjectValueAsString("installed").getValue();
-        return new SteamGameMetadata(gameId, steamApp.getName(), installedValue.equals("1"));
+        return new SteamGameMetadata(gameId, steamApp.getName(), getInstalledAppIds().contains(gameId));
     }
 
     private RegistryObject getAppsRegistryObject() {
@@ -73,5 +64,11 @@ public abstract class FileBasedRegistry extends SteamRegistry {
 
     private RegistryObject getRegistryObject(long gameId) {
         return getAppsRegistryObject().getObjectValueAsObject("" + gameId);
+    }
+
+    private boolean isInstalled(Long gameId) {
+        RegistryObject registryObject = getRegistryObject(gameId);
+        return registryObject.pathExists("installed") &&
+                registryObject.getObjectValueAsString("installed").getValue().equals("1");
     }
 }
