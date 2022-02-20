7f9bc1cc8fb2468ce5097af7a5604124746935ec
Alex Selesse <alex@selesse.com>
2022-02-20 13:45:55 -0500

    Remove unused PrintAppInfo
    This has all been obsoleted by the new GameLoading strategies

--- steam-config-reader/src/main/java/com/selesse/steam/steamcmd/PrintAppInfoExecutor.java
+++ steam-config-reader/src/main/java/com/selesse/steam/steamcmd/PrintAppInfoExecutor.java
@@ -1,15 +1,12 @@
 package com.selesse.steam.steamcmd;
 
 import com.google.common.base.Splitter;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
 import com.selesse.os.OperatingSystems;
 import com.selesse.processes.ProcessRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
-import java.util.Map;
 
 class PrintAppInfoExecutor {
     private static final Logger LOGGER = LoggerFactory.getLogger(PrintAppInfoExecutor.class);
@@ -29,44 +26,5 @@ class PrintAppInfoExecutor {
         int firstLine = lines.indexOf(String.format("\"%d\"", appId));
         int lastLine = lines.lastIndexOf("}") + 1;
         return lines.subList(firstLine, lastLine);
-    }
-
-    public Map<Long, List<String>> runPrintAppInfoProcesses(List<Long> appIds) {
-        List<String> commands = Lists.newArrayList("steamcmd", "+login", "anonymous");
-        for (Long appId : appIds) {
-            commands.add("+app_info_print");
-            commands.add(appId.toString());
-        }
-        commands.add("+quit");
-        ProcessRunner processRunner = new ProcessRunner(commands.toArray(new String[0]));
-        String output = processRunner.runAndGetOutput();
-
-        List<String> outputLines = Splitter.on("\n").splitToList(output);
-
-        Map<Long, List<String>> outputPerAppId = Maps.newHashMap();
-
-        for (Long appId : appIds) {
-            for (int i = 0; i < outputLines.size(); i++) {
-                String line = outputLines.get(i);
-                if (line.equals(String.format("\"%d\"", appId))) {
-                    int nextEndBlock = findNextEndBlock(i, outputLines);
-
-                    outputPerAppId.put(appId, outputLines.subList(i, nextEndBlock));
-                    break;
-                }
-            }
-        }
-
-        return outputPerAppId;
-    }
-
-    private int findNextEndBlock(int startingPosition, List<String> outputLines) {
-        for (int i = startingPosition; i < outputLines.size(); i++) {
-            String line = outputLines.get(i);
-            if (line.equals("}")) {
-                return i + 1;
-            }
-        }
-        return -1;
     }
 }
