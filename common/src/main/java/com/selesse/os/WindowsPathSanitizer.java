2173f0e42a376d836568a66b06216b4502dab918
Alex Selesse <alex@selesse.com>
2022-03-02 19:54:31 -0500

    Support replacing %PROGRAMFILES(X86)%

--- common/src/main/java/com/selesse/os/WindowsPathSanitizer.java
+++ common/src/main/java/com/selesse/os/WindowsPathSanitizer.java
@@ -6,6 +6,8 @@ class WindowsPathSanitizer {
     public static String sanitize(String input) {
         return OsAgnosticPaths.of(input.replace("%USERPROFILE%", System.getenv("userprofile"))
                 .replace("%LOCALAPPDATA%", System.getenv("localappdata"))
-                .replace("%APPDATA%", System.getenv("appdata")));
+                .replace("%APPDATA%", System.getenv("appdata"))
+                .replace("%PROGRAMFILES(X86)%", System.getenv("programfiles(x86)"))
+                .replace("%PROGRAMFILES%", System.getenv("programfiles")));
     }
 }
