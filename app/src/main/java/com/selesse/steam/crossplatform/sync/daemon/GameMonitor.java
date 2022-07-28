1f9d728d9896f4fa0b38fab205d8403fb4acf95c
Alex Selesse <alex@selesse.com>
2022-07-28 08:48:48 -0400

    Move GameOverlay process locator into its own file
    I want to make this work for Steam OS, so this will make that change
    easier to perform.

--- app/src/main/java/com/selesse/steam/crossplatform/sync/daemon/GameMonitor.java
+++ app/src/main/java/com/selesse/steam/crossplatform/sync/daemon/GameMonitor.java
@@ -1,15 +1,10 @@
 package com.selesse.steam.crossplatform.sync.daemon;
 
-import com.google.common.base.Splitter;
-import com.google.common.collect.Lists;
-import com.selesse.os.OperatingSystems;
 import com.selesse.steam.GameRunningDetector;
 import com.selesse.steam.crossplatform.sync.SteamCrossplatformSyncContext;
 import com.selesse.steam.crossplatform.sync.SyncGameFilesService;
 import com.selesse.steam.games.SteamGame;
-import java.io.File;
-import java.util.List;
-import java.util.Optional;
+import com.selesse.steam.processes.GameOverlayProcessLocator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -52,30 +47,9 @@ public class GameMonitor implements Runnable {
     private void onGameLaunch(SteamGame runningGame) {
         LOGGER.info("Game launched: {}", runningGame.getName());
 
-        findGameOverlayProcess()
+        GameOverlayProcessLocator.locate()
                 .ifPresentOrElse(
                         processHandle -> processHandle.onExit().thenRunAsync(this),
-                        () -> LOGGER.info("Couldn't find GameOverlay process"));
-    }
-
-    private Optional<ProcessHandle> findGameOverlayProcess() {
-        return ProcessHandle.allProcesses()
-                .filter(ProcessHandle::isAlive)
-                .filter(p -> {
-                    String gameOverlayProcess = getGameOverlayProcessName();
-                    List<String> processArguments = p.info()
-                            .command()
-                            .map(x -> Splitter.on(File.separatorChar).splitToList(x))
-                            .orElse(Lists.newArrayList());
-                    return processArguments.size() > 0
-                            && processArguments.get(processArguments.size() - 1).equalsIgnoreCase(gameOverlayProcess);
-                })
-                .findFirst();
-    }
-
-    private String getGameOverlayProcessName() {
-        return OperatingSystems.get() == OperatingSystems.OperatingSystem.WINDOWS
-                ? "GameOverlayUI.exe"
-                : "gameoverlayui";
+                        () -> LOGGER.info("Couldn't find game overlay process"));
     }
 }
---
+++ steam-config-reader/src/main/java/com/selesse/steam/processes/GameOverlayProcessLocator.java
@@ -0,0 +1,32 @@
+package com.selesse.steam.processes;
+
+import com.google.common.base.Splitter;
+import com.google.common.collect.Lists;
+import com.selesse.os.OperatingSystems;
+import java.io.File;
+import java.util.List;
+import java.util.Optional;
+
+public class GameOverlayProcessLocator {
+    public static Optional<ProcessHandle> locate() {
+        return ProcessHandle.allProcesses()
+                .filter(ProcessHandle::isAlive)
+                .filter(p -> {
+                    String gameOverlayProcess = getGameOverlayProcessName();
+                    List<String> processArguments = p.info()
+                            .command()
+                            .map(x -> Splitter.on(File.separatorChar).splitToList(x))
+                            .orElse(Lists.newArrayList());
+                    return processArguments.size() > 0
+                            && processArguments.get(processArguments.size() - 1).equalsIgnoreCase(gameOverlayProcess);
+                })
+                .findFirst();
+    }
+
+    private static String getGameOverlayProcessName() {
+        return switch (OperatingSystems.get()) {
+            case WINDOWS -> "GameOverlayUI.exe";
+            case MAC, LINUX -> "gameoverlayui";
+        };
+    }
+}
