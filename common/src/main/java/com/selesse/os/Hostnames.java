8bbd95ad66b8dfb7e2f34163b33bf207ed3d056d
Alex Selesse <alex@selesse.com>
2022-11-19 11:44:22 -0500

    Add mechanism to get machine's hostname

---
+++ common/src/main/java/com/selesse/os/Hostnames.java
@@ -0,0 +1,29 @@
+package com.selesse.os;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Optional;
+
+public class Hostnames {
+    public static String getCurrent() {
+        try {
+            return InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException e) {
+            return Optional.ofNullable(System.getenv("COMPUTERNAME"))
+                    .orElseGet(
+                            () -> Optional.ofNullable(System.getenv("HOSTNAME")).orElse(fallbackHostname()));
+        }
+    }
+
+    private static String fallbackHostname() {
+        try {
+            Process hostnameProcess = Runtime.getRuntime().exec("hostname");
+            return new BufferedReader(new InputStreamReader(hostnameProcess.getInputStream())).readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
