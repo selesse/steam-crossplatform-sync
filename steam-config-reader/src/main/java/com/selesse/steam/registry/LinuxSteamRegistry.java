57249132210531e5c56098dd57d7adec871e78d8
Alex Selesse <alex@selesse.com>
2023-08-31 09:29:24 +0300

    Double check currently running app
    One of the new Steam OS patches stopped writing the currently running
    app ID to the registry file. As a workaround, if the registry file
    reports that no app is running, let's check the steam reaper process to
    see if there actually is an app running.

--- steam-config-reader/src/main/java/com/selesse/steam/registry/LinuxSteamRegistry.java
+++ steam-config-reader/src/main/java/com/selesse/steam/registry/LinuxSteamRegistry.java
@@ -1,11 +1,30 @@
 package com.selesse.steam.registry;
 
 import com.selesse.os.FilePathSanitizer;
+import com.selesse.steam.processes.GameOverlayProcessLocator;
+
 import java.nio.file.Path;
 
 public class LinuxSteamRegistry extends FileBasedRegistry {
     @Override
     Path registryPath() {
         return Path.of(FilePathSanitizer.sanitize("~/.steam/registry.vdf"));
+    }
+
+    @Override
+    public long getCurrentlyRunningAppId() {
+        long currentlyRunningAppId = super.getCurrentlyRunningAppId();
+        if (currentlyRunningAppId == 0) {
+            return GameOverlayProcessLocator.locate().map(reaperProcess -> {
+                var processArguments = reaperProcess.info().arguments().orElse(new String[]{});
+                for (String argument : processArguments) {
+                    if (argument.contains("AppId=")) {
+                        return Long.parseLong(argument.split("=")[1]);
+                    }
+                }
+                return 0L;
+            }).orElse(0L);
+        }
+        return currentlyRunningAppId;
     }
 }
