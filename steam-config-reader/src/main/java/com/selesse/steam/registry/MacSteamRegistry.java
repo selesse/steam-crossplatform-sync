f91e1a32eb01e49e6eace0c5bb20a3747461c284
Alex Selesse <alex@selesse.com>
2023-12-25 11:17:03 -0500

    Make Mac FileBasedRegistry fall back to game overlay process
    The file based registry is no longer a reliable way to determine what
    game is currently running. The Windows/Linux implementations had
    fallback logic to look at the game overlay process, this adds the Mac
    one.
    
    Now that all implementations rely on this, I will need to modify the
    code to no longer treat the file based registry as reliable. The
    registries will implement their own way of locating the game overlay process.

--- steam-config-reader/src/main/java/com/selesse/steam/registry/MacSteamRegistry.java
+++ steam-config-reader/src/main/java/com/selesse/steam/registry/MacSteamRegistry.java
@@ -1,11 +1,34 @@
 package com.selesse.steam.registry;
 
 import com.selesse.os.FilePathSanitizer;
+import com.selesse.steam.processes.GameOverlayProcessLocator;
 import java.nio.file.Path;
+import java.util.Arrays;
 
 class MacSteamRegistry extends FileBasedRegistry {
     @Override
     Path registryPath() {
         return Path.of(FilePathSanitizer.sanitize("~/Library/Application Support/Steam/registry.vdf"));
+    }
+
+    @Override
+    public long getCurrentlyRunningAppId() {
+        long currentlyRunningAppId = super.getCurrentlyRunningAppId();
+        // TODO: Steam stopped accurately using the registry file, convert all implementations to use the game overlay
+        // process
+        if (currentlyRunningAppId == 0) {
+            return GameOverlayProcessLocator.locate()
+                    .map(gameOverlayProcess -> {
+                        var processArguments =
+                                gameOverlayProcess.info().arguments().orElse(new String[] {});
+                        int gameIdIndex = Arrays.asList(processArguments).indexOf("-gameid");
+                        if (gameIdIndex != -1 && processArguments.length >= gameIdIndex + 1) {
+                            return Long.parseLong(processArguments[gameIdIndex + 1]);
+                        }
+                        return 0L;
+                    })
+                    .orElse(0L);
+        }
+        return currentlyRunningAppId;
     }
 }
