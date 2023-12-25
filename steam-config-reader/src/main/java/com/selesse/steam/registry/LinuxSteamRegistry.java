2d0522786a197a9fecb51e862f922a64e33e9449
Alex Selesse <alex@selesse.com>
2023-12-25 11:21:56 -0500

    Run spotlessApply

--- steam-config-reader/src/main/java/com/selesse/steam/registry/LinuxSteamRegistry.java
+++ steam-config-reader/src/main/java/com/selesse/steam/registry/LinuxSteamRegistry.java
@@ -2,7 +2,6 @@ package com.selesse.steam.registry;
 
 import com.selesse.os.FilePathSanitizer;
 import com.selesse.steam.processes.GameOverlayProcessLocator;
-
 import java.nio.file.Path;
 
 public class LinuxSteamRegistry extends FileBasedRegistry {
@@ -15,15 +14,17 @@ public class LinuxSteamRegistry extends FileBasedRegistry {
     public long getCurrentlyRunningAppId() {
         long currentlyRunningAppId = super.getCurrentlyRunningAppId();
         if (currentlyRunningAppId == 0) {
-            return GameOverlayProcessLocator.locate().map(reaperProcess -> {
-                var processArguments = reaperProcess.info().arguments().orElse(new String[]{});
-                for (String argument : processArguments) {
-                    if (argument.contains("AppId=")) {
-                        return Long.parseLong(argument.split("=")[1]);
-                    }
-                }
-                return 0L;
-            }).orElse(0L);
+            return GameOverlayProcessLocator.locate()
+                    .map(reaperProcess -> {
+                        var processArguments = reaperProcess.info().arguments().orElse(new String[] {});
+                        for (String argument : processArguments) {
+                            if (argument.contains("AppId=")) {
+                                return Long.parseLong(argument.split("=")[1]);
+                            }
+                        }
+                        return 0L;
+                    })
+                    .orElse(0L);
         }
         return currentlyRunningAppId;
     }
